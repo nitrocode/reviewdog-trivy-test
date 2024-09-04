efddc6794625e3a6b70308783b1e3d92673f6ef6
nitrocode <7775707+nitrocode@users.noreply.github.com>
2024-09-04 11:18:42 -0500

    fix: add new bucket

--- main.tf
+++ main.tf
@@ -19,3 +19,22 @@ module "s3_bucket_2" {
 
   kms_master_key_arn = var.kms_master_key_arn
 }
+
+module "s3_bucket_3" {
+  source = "cloudposse/s3-bucket/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version = "x.x.x"
+  name = var.name
+
+  enabled = true
+
+  s3_object_ownership = "BucketOwnerEnforced"
+  versioning_enabled  = true
+
+  logging = [{
+    bucket_name = "app"
+    prefix      = "logs/"
+  }]
+
+  kms_master_key_arn = var.kms_master_key_arn
+}
